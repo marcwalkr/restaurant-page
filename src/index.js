90799e9575ab04930ad690d49975f86e5a76242f
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2025-10-20 14:35:15 -0500

    Highlight navigation tab on click
    - Add ids to navigation elements for querying in JavaScript
    - Make tabs into buttons and add role attributes for accessibility
    - Add cursor: pointer to tab buttons so it behaves like a link
    - Add class for changing the background color when tab is selected
    - Implement click listener to change background color on tab click

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import loadHome from "./home.js";
 import "./styles.css";
 
+const navigation = document.querySelector("#navigation");
+const home = navigation.querySelector("#home");
+const menu = navigation.querySelector("#menu");
+const contact = navigation.querySelector("#contact");
+
+const deselectTabs = () => {
+  home.classList.remove("header__nav-link--selected");
+  menu.classList.remove("header__nav-link--selected");
+  contact.classList.remove("header__nav-link--selected");
+}
+
+navigation.addEventListener("click", (event) => {
+  const button = event.target.closest("li");
+  if (!button) return;
+
+  deselectTabs();
+  button.classList.add("header__nav-link--selected");
+});
+
 loadHome();
