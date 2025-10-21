d4db55e6a12f5d56909490316cbe5237774a4e49
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2025-10-21 11:58:18 -0500

    Implement page load on tab click
    - Add helper functions for handling tab highlighting
    - Add tabSelected function to check if a tab is already selected
    - Add clearContent helper function
    - Add menu and contact modules with empty load functions
    - Call the respective page's load function on tab click
    - Handle not reloading the page if the tab is already selected
    - Load the home page on initial page load

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import loadHome from "./home.js";
+import loadMenu from "./menu.js";
+import loadContact from "./contact.js";
 import "./styles.css";
 
+const content = document.querySelector("#content");
 const navigation = document.querySelector("#navigation");
 const home = navigation.querySelector("#home");
 const menu = navigation.querySelector("#menu");
 const contact = navigation.querySelector("#contact");
+
+const selectTab = (tab) => {
+  tab.classList.add("header__nav-link--selected");
+}
 
 const deselectTabs = () => {
   home.classList.remove("header__nav-link--selected");
@@ -12,12 +19,34 @@ const deselectTabs = () => {
   contact.classList.remove("header__nav-link--selected");
 }
 
+const tabSelected = (tab) => {
+  return tab.classList.contains("header__nav-link--selected");
+}
+
+const clearContent = () => {
+  content.textContent = "";
+}
+
 navigation.addEventListener("click", (event) => {
-  const button = event.target.closest("button");
-  if (!button) return;
+  const tab = event.target.closest("button");
+  if (!tab || tabSelected(tab)) return;
 
   deselectTabs();
-  button.classList.add("header__nav-link--selected");
+  selectTab(tab);
+  clearContent();
+
+  switch (tab.id) {
+    case "home":
+      loadHome();
+      break;
+    case "menu":
+      loadMenu();
+      break;
+    case "contact":
+      loadContact();
+      break;
+  }
 });
 
+selectTab(home);
 loadHome();
